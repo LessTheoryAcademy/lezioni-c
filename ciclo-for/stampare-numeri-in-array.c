04b5b05d7f41327ff1149ef8e946441f38e361d9
LessTheoryAcademy <73408559+LessTheoryAcademy@users.noreply.github.com>
2022-06-26 21:53:09 +0200

    Update stampare-numeri-in-array.c

--- ciclo-for/stampare-numeri-in-array.c
+++ ciclo-for/stampare-numeri-in-array.c
@@ -5,34 +5,16 @@ int main() {
 	
 	int numbers[] = { 19, 10, 8, 17, 9, 2, 5, 2, 10, 4, 10, 2, 10, 10, 13, 15 };
 	
-	/*
-	
-		0 : 19
-		1 : 10
-		2 : 8
-		...
-		13 : 10
-		
-		lunghezza : 14
-	
-	*/
-	
 	int contatore;
 	
 	int lunghezzaArray = sizeof numbers / sizeof numbers[0];
 	
-	for(contatore = 0; contatore <= (lunghezzaArray - 1); contatore++) {
+	for(contatore = 0; contatore < lunghezzaArray; contatore++) {
 		
 		int numeroTrovato = numbers[contatore];
 		
 		printf("Contatore vale %d Numero trovato vale %d\n", contatore, numeroTrovato);
 	}
-	
-	// int numbers[] = { 19, 10, 8, 17, 9, 2, 5, 2, 10, 4, 10, 2, 10, 10 };
-	
-	// 0...13
-	
-	//printf("Numero trovato vale %d\n", numbers[indice]);
    
 	return 0;
 }
